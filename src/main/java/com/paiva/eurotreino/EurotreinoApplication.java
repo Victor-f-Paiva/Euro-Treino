d746a66c51c5dd2a53e36b136b03689351d29775
Victor Paiva <victor_eduardof@hotmail.com>
2025-07-17 18:31:29 -0300

    chore: first commit

---
+++ src/main/java/com/paiva/eurotreino/EurotreinoApplication.java
@@ -0,0 +1,13 @@
+package com.paiva.eurotreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurotreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurotreinoApplication.class, args);
+	}
+
+}
