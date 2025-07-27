eb0d5b3dde63a47a00c3612a7a787efdfcad0a1f
Victor Paiva <victor_eduardof@hotmail.com>
2025-07-27 08:10:48 -0300

    test(repository, resources): test userRepository. add application-test-example.properties

---
+++ src/test/java/com/paiva/eurotreino/repository/UserRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.paiva.eurotreino.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.paiva.eurotreino.entity.Exercise;
+import com.paiva.eurotreino.entity.ExerciseSession;
+import com.paiva.eurotreino.entity.Macro;
+import com.paiva.eurotreino.entity.Meso;
+import com.paiva.eurotreino.entity.Micro;
+import com.paiva.eurotreino.entity.Set;
+import com.paiva.eurotreino.entity.User;
+import com.paiva.eurotreino.entity.Workout;
+import com.paiva.eurotreino.enums.MuscularGroup;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void testPersistComplete() {
+		
+		Exercise supinoReto = new Exercise("Supino reto", MuscularGroup.PECS, MuscularGroup.TRICEPS);
+		Set set1SR = new Set(1, 10, 50); 
+		Set set2SR = new Set(1, 10, 50); 
+		Set set3SR = new Set(1, 10, 50); 
+		
+		Exercise supinoInclinado = new Exercise("Supino inclinado", MuscularGroup.PECS, MuscularGroup.TRICEPS);
+		Set set1SI = new Set(1, 10, 40); 
+		Set set2SI = new Set(1, 10, 40); 
+		Set set3SI = new Set(1, 10, 40); 
+		
+		ExerciseSession supinoRetoW = new ExerciseSession(supinoReto, List.of(set1SR, set2SR, set3SR));
+		ExerciseSession supinoInclinadoW = new ExerciseSession(supinoInclinado, List.of(set1SI, set2SI, set3SI));
+		
+		Workout treinoA = new Workout("Peito e triceps", List.of(supinoRetoW, supinoInclinadoW));
+		
+		Micro micro1 = new Micro(LocalDate.now(), List.of(treinoA));
+		
+		Meso meso1 = new Meso(LocalDate.now(), List.of(micro1));
+		
+		Macro macro1 = new Macro(LocalDate.now(), List.of(meso1));
+		
+		User user1 = new User("Victor Paiva", "victor@email.com", List.of(macro1));
+		
+		User saved = userRepository.save(user1);
+		
+		assertThat(saved.getId()).isNotNull();
+		assertThat(saved.getMacroCycles()).hasSize(1);
+		assertThat(saved.getMacroCycles().get(0).getMesoCycles()).hasSize(1);
+		assertThat(saved.getMacroCycles().get(0).getMesoCycles().get(0).getMicroCycles()).hasSize(1);
+		assertEquals(2700, saved.getMacroCycles().get(0)
+				.getMesoCycles().get(0)
+				.getMicroCycles().get(0)
+				.getGroupVolume(MuscularGroup.PECS));
+		
+	}
+
+}
