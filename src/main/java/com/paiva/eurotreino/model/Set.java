bfada757d08c9f02c45cf1dc48944e32d82ea0e1
Victor Paiva <victor_eduardof@hotmail.com>
2025-08-20 15:50:39 -0300

    refactor(set): change table name to 'tb_set' and change param 'order' to 'exerciseOrder', to avoid conflict with SQL reserved words

--- src/main/java/com/paiva/eurotreino/model/Set.java
+++ src/main/java/com/paiva/eurotreino/model/Set.java
@@ -6,23 +6,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  * Represents a set (series) of an exercise, including reps and weight.
- * Each set has a specific order, number of repetitions, and weight used.
+ * Each set has a specific exerciseOrder, number of repetitions, and weight used.
  * It belongs to an ExerciseSession.
  */
 @Entity
+@Table(name= "tb_set")
 @Getter @Setter @NoArgsConstructor
 public class Set {
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
-	private int order;
+	private int exerciseOrder;
 	private int reps;
 	private double weight;
 	
@@ -33,13 +35,13 @@ public class Set {
 	   /**
      * Constructs a Set with all required fields.
      *
-     * @param order the set order in the exercise session
+     * @param exerciseOrder the set exerciseOrder in the exercise session
      * @param reps the number of repetitions
      * @param weight the weight used in the set
      * @param exerciseSession the parent ExerciseSession
      */
-    public Set(int order, int reps, double weight) {
-        this.order = order;
+    public Set(int exerciseOrder, int reps, double weight) {
+        this.exerciseOrder = exerciseOrder;
         this.reps = reps;
         this.weight = weight;
     }
