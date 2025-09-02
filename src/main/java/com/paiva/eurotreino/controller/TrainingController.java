ef01cad908519114ae2f66029ad6029fdb2eca4a
Victor Paiva <victor_eduardof@hotmail.com>
2025-09-02 06:21:34 -0300

    feat(TrainingController): add TrainingController with the endpoint to add workout

---
+++ src/main/java/com/paiva/eurotreino/controller/TrainingController.java
@@ -0,0 +1,28 @@
+package com.paiva.eurotreino.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paiva.eurotreino.model.Workout;
+import com.paiva.eurotreino.service.TrainingService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(value = "training")
+@AllArgsConstructor
+public class TrainingController {
+
+    private TrainingService trainingService;
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> addWorkout(@PathVariable Long userId, @RequestBody Workout workout){
+        trainingService.addWorkout(userId, workout);
+        return ResponseEntity.noContent().build();
+    }
+
+}
