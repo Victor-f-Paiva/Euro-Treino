c9638187f048303f0da8574df7e32ca974a1464c
Victor Paiva <victor_eduardof@hotmail.com>
2025-09-06 10:57:02 -0300

    fix(TrainingController): change endpoint to POST to better represent resource creation

--- src/main/java/com/paiva/eurotreino/controller/TrainingController.java
+++ src/main/java/com/paiva/eurotreino/controller/TrainingController.java
@@ -2,7 +2,7 @@ package com.paiva.eurotreino.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ public class TrainingController {
 
     private TrainingService trainingService;
 
-    @PutMapping("/{userId}")
+    @PostMapping("/{userId}")
     public ResponseEntity<Void> addWorkout(@PathVariable Long userId, @RequestBody Workout workout){
         trainingService.addWorkout(userId, workout);
         return ResponseEntity.noContent().build();
