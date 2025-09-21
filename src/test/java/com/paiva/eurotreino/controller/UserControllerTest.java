6114109623c4fe0fcbf3828d4c9cd153f50bbd4b
Victor Paiva <victor_eduardof@hotmail.com>
2025-09-21 09:21:45 -0300

    test(UserControllerTest): test UserControllerTest new method to visualize the workout by user id

--- src/test/java/com/paiva/eurotreino/controller/UserControllerTest.java
+++ src/test/java/com/paiva/eurotreino/controller/UserControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -38,13 +39,14 @@ class UserControllerTest {
     private UserService userService; // mockado, diferente do teste de service
 
     private User user1;
+    private Workout treinoA;
 
     @BeforeEach
     void setUp() {
         Exercise supinoReto = new Exercise("Supino reto", MuscularGroup.PECS, MuscularGroup.TRICEPS);
         Set set1SR = new Set(1, 10, 50);
         ExerciseSession supinoRetoW = new ExerciseSession(supinoReto, List.of(set1SR));
-        Workout treinoA = new Workout("Peito e triceps", List.of(supinoRetoW));
+        treinoA = new Workout("Peito e triceps", new ArrayList<>(List.of(supinoRetoW)));
 
         Micro micro1 = new Micro(LocalDate.now(), List.of(treinoA));
         Meso meso1 = new Meso(LocalDate.now(), List.of(micro1));
@@ -116,5 +118,27 @@ class UserControllerTest {
 
         mockMvc.perform(delete("/users/1"))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void testVisualizeWorkoutSuccess() throws Exception {
+        when(userService.visualizeWorkout(1L)).thenReturn(treinoA);
+
+        System.out.println("Teste: " + userService.visualizeWorkout(1L));
+
+        mockMvc.perform(get("/users/workout/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Peito e triceps"))
+                .andExpect(jsonPath("$.exercises[0].exercise.name").value("Supino reto"))
+                .andExpect(jsonPath("$.exercises[0].sets[0].reps").value(10))
+                .andExpect(jsonPath("$.exercises[0].sets[0].weight").value(50.0));
+    }
+
+    @Test
+    void testVisualizeWorkoutNotFound() throws Exception {
+        when(userService.visualizeWorkout(99L)).thenThrow(new NotFoundException("User 99 has no workout found."));
+
+        mockMvc.perform(get("/users/workout/99"))
+                .andExpect(status().isNotFound());
     }
 }
